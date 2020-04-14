15fd355e543fdcfe2b6a5185c7d40f858b348eb8
clf <1256233771@qq.com>
2020-04-14 19:33:24 +0800

    fix bug

--- user-server/src/main/java/com/clf/cloud/userserver/UserServerApplication.java
+++ user-server/src/main/java/com/clf/cloud/userserver/UserServerApplication.java
@@ -1,5 +1,6 @@
 package com.clf.cloud.userserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,6 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("com.clf.cloud.userserver.dao")
 @ComponentScan(basePackages = {"com.clf.cloud", "org.n3r.idworker"})
 public class UserServerApplication {
 
